281b5ab5017196a8b3ee4b3343fdf3e73eb63f47
Lubna Khrais <khrais.lubna@outlook.com>
2026-09-20 22:39:08 +0300

    Complete Task 4 branding integration

--- bike_management/__manifest__.py
+++ bike_management/__manifest__.py
@@ -27,25 +27,26 @@
     'assets': {
         'web.assets_backend': [
             'bike_management/static/src/scss/rtl.scss',
+            'bike_management/static/src/scss/branding.scss',
+        ],
+        'web.assets_frontend': [
+            'bike_management/static/src/scss/branding.scss',
         ],
     },
 
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
-	'views/rtl_views.xml',
+        'views/rtl_views.xml',
         'views/bike_views.xml',
         'views/repair_views.xml',
         'views/partner_views.xml',
         'data.xml',
-
         'report/rental_report.xml',
-
         'views/product_views.xml',
         'views/dashboard_views.xml',
         'views/portal_templates.xml',
         'views/rental_views.xml',
-
         'views/rental_analysis_views.xml',
     ],
 
